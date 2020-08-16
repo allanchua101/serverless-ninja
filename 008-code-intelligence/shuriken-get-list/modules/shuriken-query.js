25b31b47571408956b9d7980725826ec1a0cf4a9
Allan Chua <allanchua.officefiles@gmail.com>
2020-08-16 12:36:37 +0800

    More code intelligence demos

---
+++ 008-code-intelligence/shuriken-get-list/modules/shuriken-query.js
@@ -0,0 +1,30 @@
+const AWS = require("aws-sdk");
+const options = {
+  apiVersion: "2012-08-10",
+  region: process.env.APP_REGION || "ap-southeast-1",
+};
+const client = new AWS.DynamoDB.DocumentClient(options);
+
+module.exports = {
+  /**
+   * @async
+   * @function getShurikens
+   * @description Method used for retrieving list of shuriken.
+   * @returns {Promise<object[]>} List of recorded shuriken.
+   */
+  async getShurikens() {
+    let params = {
+      TableName: process.env.SHURIKEN_TABLE,
+    };
+    let output = [];
+
+    do {
+      let data = await client.scan(params).promise();
+
+      output = output.concat(...data.Items);
+      params.ExclusiveStartKey = data.LastEvaluatedKey;
+    } while (typeof params.ExclusiveStartKey != "undefined");
+
+    return output;
+  },
+};
