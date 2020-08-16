95f3af22f180700866b69bb0ae500408192c0d52
Allan Chua <allanchua.officefiles@gmail.com>
2020-08-16 12:32:47 +0800

    Created apis for dagger and katanas

---
+++ 008-code-intelligence/dagger-get-list/app.js
@@ -0,0 +1,33 @@
+const { getDaggers } = require("./modules/dagger-query");
+
+let respond = (statusCode, body) => {
+  return {
+    statusCode: statusCode,
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type,x-api-key",
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET,DELETE",
+    },
+  };
+};
+
+module.exports = {
+  async execute(event, context) {
+    if (event.httpMethod.toLowerCase() === "options") {
+      return respond(200, {});
+    }
+
+    let items = [];
+
+    try {
+      items = await getDaggers();
+
+      return respond(200, { items });
+    } catch (err) {
+      // TODO: Add logging code here
+      return respond(500, { issue: "Internal server error" });
+    }
+  },
+};
