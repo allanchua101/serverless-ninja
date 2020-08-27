31f1177b857a08288033cd6ead6ebda0f64b93b4
Allan Chua <allanchua.officefiles@gmail.com>
2020-08-27 18:20:21 +0800

    Typo fix

--- 006-ai-apis/001-classifiers/002_evaluate.js
+++ 006-ai-apis/001-classifiers/002_evaluate.js
@@ -4,5 +4,5 @@ const naiveBayes = natural.BayesClassifier;
 naiveBayes.load("./hate-speech-classifier.json", null, (err, classifier) => {
   console.log(classifier.classify("Who likes a Mcdonalds dress anyway"));
   console.log(classifier.classify("I never liked this bitch"));
-  console.log(classifier.classify("Are you crazy our out of your mind?"));
+  console.log(classifier.classify("Are you crazy or out of your mind?"));
 });
